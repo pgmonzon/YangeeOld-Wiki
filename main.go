b588ad3d71c64a66a0cbd19e58eeb95f11c91a46
Patricio Garcia Monzon <pgmonzon@gmail.com>
2018-04-02 13:15:25 -0300

    appengine

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 
   "github.com/pgmonzon/Yangee/routers"
   "github.com/pgmonzon/Yangee/config"
+
+  "google.golang.org/appengine"
 )
 
 func main() {
+  appengine.Main()
+  
   fmt.Println(" ***********************")
   fmt.Println(" ¡¡¡ Wooow is Yangee !!!")
   fmt.Println(" ***********************")
