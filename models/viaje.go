1d3f36645f966e939676e42e2d42be618f678676
Patricio Garcia Monzon <pgmonzon@gmail.com>
2018-05-21 10:19:15 -0300

    omitempty

--- models/viaje.go
+++ models/viaje.go
@@ -8,6 +8,96 @@ import (
 
 type Viaje struct {
 	ID							          bson.ObjectId	 	`bson:"_id" json:"id,omitempty"`
+	Empresa_id			          bson.ObjectId		`bson:"empresa_id" json:"empresa_id"`
+  FechaHora                 time.Time       `bson:"fechaHora" json:"fechaHora"`
+  Cliente_id	   	          bson.ObjectId		`bson:"cliente_id" json:"cliente_id"`
+  Cliente                   string          `bson:"cliente" json:"cliente"`
+  TipoUnidad_id	 	          bson.ObjectId		`bson:"tipoUnidad_id" json:"tipoUnidad_id"`
+  TipoUnidad                string          `bson:"tipoUnidad" json:"tipoUnidad"`
+  Transportista_id	 	      bson.ObjectId		`bson:"transportista_id" json:"transportista_id"`
+  Transportista             string          `bson:"transportista" json:"transportista"`
+  Unidad_id	        	      bson.ObjectId		`bson:"unidad_id" json:"unidad_id"`
+  Unidad                    string          `bson:"unidad" json:"unidad"`
+  Personal_id	   	          bson.ObjectId		`bson:"personal_id" json:"personal_id"`
+  Personal                  string          `bson:"personal" json:"personal"`
+  Paradas	    		  	      []ParadasServ		`bson:"paradas" json:"paradas"`
+  Recorrido                 string          `bson:"recorrido" json:"recorrido"` // La primera y la última parada indicando si hay intermedias
+  Kilometraje               int             `bson:"kilometraje" json:"kilometraje"`
+  TarifarioCliente          string          `bson:"tarifarioCliente" json:"tarifarioCliente"`
+  TarifaValor               float64         `bson:"tarifaValor" json:"tarifaValor"`
+  ValorViaje                float64         `bson:"valorViaje" json:"valorViaje"`
+  AutValorViaje_id          bson.ObjectId   `bson:"autValorViaje_id" json:"autValorViaje_id"` // el usuario que autorizó la tarifa
+  AutValor                  string          `bson:"autValor" json:"autValor"` // usuario que autorizó
+  AutValorViajeFecha        time.Time       `bson:"autValorViajeFecha" json:"autValorViajeFecha"`
+  TarifarioTransportista    string          `bson:"tarifarioTransportista" json:"tarifarioTransportista"`
+  TarifaCosto               float64         `bson:"tarifaCosto" json:"tarifaCosto"`
+  CostoViaje                float64         `bson:"costoViaje" json:"costoViaje"`
+  AutCostoViaje_id          bson.ObjectId   `bson:"autCostoViaje_id" json:"autCostoViaje_id"` // el usuario que autorizó la tarifa
+  AutCosto                  string          `bson:"autCosto" json:"autCosto"` // usuario que autorizó
+  AutCostoViajeFecha        time.Time       `bson:"autCostoViajeFecha" json:"autCostoViajeFecha"`
+  Peajes                    float64         `bson:"peajes" json:"peajes"`
+  Observaciones             string          `bson:"observaciones" json:"observaciones"`
+  Estado                    string          `bson:"estado" json:"estado"` // Ok - Sin Tarifa - Cancelado - Cerrado
+  Cancelado_id              bson.ObjectId   `bson:"cancelado_id" json:"cancelado_id"` // el usuario que canceló
+  CanceladoUsuario          string          `bson:"canceladoUsuario" json:"canceladoUsuario"`
+  CanceladoFecha            time.Time       `bson:"canceladoFecha" json:"canceladoFecha"`
+  CanceladoObser            string          `bson:"canceladoObser" json:"canceladoObser"`
+  Remitos                   bool            `bson:"remitos" json:"remitos"`
+  Remitos_id                bson.ObjectId   `bson:"remitos_id" json:"remitos_id"` // usuario que recibió los remitos
+  RemitosUsuario            string          `bson:"remitosUsuario" json:"remitosUsuario"`
+  RemitosFecha              time.Time       `bson:"remitosFecha" json:"remitosFecha"`
+  Factura_id                bson.ObjectId   `bson:"factura_id" json:"factura_id"` // id de la factura
+  Factura                   string          `bson:"factura" json:"factura"` // a-006-154648
+  FechaFacturacion          time.Time       `bson:"fechaFacturacion" json:"fechaFacturacion"`
+  UsuarioFacturacion_id     bson.ObjectId   `bson:"usuarioFacturacion_id" json:"usuarioFacturacion_id"`
+  UsuarioFacturacion        string          `bson:"usuarioFacturacion" json:"usuarioFacturacion"`
+  Liquidacion_id            bson.ObjectId   `bson:"liquidacion_id" json:"liquidacion_id"` // id de la factura
+  Liquidacion               string          `bson:"liquidacion" json:"liquidacion"` // 478
+  FechaLiquidacion          time.Time       `bson:"fechaLiquidacion" json:"fechaLiquidacion"`
+  UsuarioLiquidacion_id     bson.ObjectId   `bson:"usuarioLiquidacion_id" json:"usuarioLiquidacion_id"`
+  UsuarioLiquidacion        string          `bson:"usuarioLiquidacion" json:"usuarioLiquidacion"`
+  Editable                  bool            `bson:"editable" json:"editable"`
+	Timestamp	     			      time.Time				`bson:"timestamp" json:"timestamp"`
+}
+
+type ParadasServ struct {
+	Locacion_id			bson.ObjectId		`bson:"locacion_id" json:"locacion_id,omitempty"`
+  Locacion        string          `bson:"locacion" json:"locacion"`
+}
+
+type CanceladoObser struct {
+	Observacion			string					`bson:"observacion" json:"observacion"`
+}
+
+type Autorizaciones struct {
+	ID							          bson.ObjectId	 	`bson:"_id" json:"id,omitempty"`
+	Empresa_id			          bson.ObjectId		`bson:"empresa_id" json:"empresa_id"`
+	Viaje_id			            bson.ObjectId   `bson:"viaje_id" json:"viaje_id"`
+	FechaHora                 time.Time       `bson:"fechaHora" json:"fechaHora"`
+	Recorrido                 string          `bson:"recorrido" json:"recorrido"` // La primera y la última parada indicando si hay intermedias
+	Kilometraje               int             `bson:"kilometraje" json:"kilometraje"`
+	Solicitante_id            bson.ObjectId   `bson:"solicitante_id" json:"solicitante_id"`
+  Solicitante		            string          `bson:"solicitante" json:"solicitante"`
+  SolicitanteFecha          time.Time       `bson:"solicitanteFecha" json:"solicitanteFecha"`
+	TipoSolicitud							string					`bson:"tipoSolicitud" json:"tipoSolicitud"` // Tarifa Cliente - Tarifa Transportista
+	Titular_id				 	      bson.ObjectId		`bson:"titular_id" json:"titular_id"`
+  Titular			         	    string          `bson:"titular" json:"titular"`
+	ImporteTarifario					float64         `bson:"importeTarifario" json:"importeTarifario"`
+	ImporteSugerido						float64         `bson:"importeSugerido" json:"importeSugerido"`
+	Autorizante_id            bson.ObjectId   `bson:"autorizante_id" json:"solicitante_id"`
+  Autorizante		            string          `bson:"autorizante" json:"autorizante"`
+  AutorizanteFecha          time.Time       `bson:"autorizanteFecha" json:"autorizanteFecha"`
+	ImporteAutorizado					float64         `bson:"importeAutorizado" json:"importeAutorizado"`
+	Timestamp	     			      time.Time				`bson:"timestamp" json:"timestamp"`
+}
+
+type ImporteSugerido struct {
+	Importe										float64         `bson:"importe" json:"importe"`
+}
+
+/*
+type Viaje struct {
+	ID							          bson.ObjectId	 	`bson:"_id" json:"id,omitempty"`
 	Empresa_id			          bson.ObjectId		`bson:"empresa_id" json:"empresa_id,omitempty"`
   FechaHora                 time.Time       `bson:"fechaHora" json:"fechaHora"`
   Cliente_id	   	          bson.ObjectId		`bson:"cliente_id,omitempty" json:"cliente_id,omitempty"`
@@ -60,37 +150,4 @@ type Viaje struct {
 	Timestamp	     			      time.Time				`bson:"timestamp,omitempty" json:"timestamp"`
 }
 
-type ParadasServ struct {
-	Locacion_id			bson.ObjectId		`bson:"locacion_id" json:"locacion_id,omitempty"`
-  Locacion        string          `bson:"locacion" json:"locacion"`
-}
-
-type CanceladoObser struct {
-	Observacion			string					`bson:"observacion" json:"observacion"`
-}
-
-type Autorizaciones struct {
-	ID							          bson.ObjectId	 	`bson:"_id" json:"id,omitempty"`
-	Empresa_id			          bson.ObjectId		`bson:"empresa_id" json:"empresa_id,omitempty"`
-	Viaje_id			            bson.ObjectId   `bson:"viaje_id" json:"viaje_id"`
-	FechaHora                 time.Time       `bson:"fechaHora" json:"fechaHora"`
-	Recorrido                 string          `bson:"recorrido" json:"recorrido,omitempty"` // La primera y la última parada indicando si hay intermedias
-	Kilometraje               int             `bson:"kilometraje" json:"kilometraje"`
-	Solicitante_id            bson.ObjectId   `bson:"solicitante_id" json:"solicitante_id,omitempty"`
-  Solicitante		            string          `bson:"solicitante" json:"solicitante,omitempty"`
-  SolicitanteFecha          time.Time       `bson:"solicitanteFecha" json:"solicitanteFecha,omitempty"`
-	TipoSolicitud							string					`bson:"tipoSolicitud" json:"tipoSolicitud,omitempty"` // Tarifa Cliente - Tarifa Transportista
-	Titular_id				 	      bson.ObjectId		`bson:"titular_id" json:"titular_id,omitempty"`
-  Titular			         	    string          `bson:"titular" json:"titular"`
-	ImporteTarifario					float64         `bson:"importeTarifario" json:"importeTarifario,omitempty"`
-	ImporteSugerido						float64         `bson:"importeSugerido" json:"importeSugerido,omitempty"`
-	Autorizante_id            bson.ObjectId   `bson:"autorizante_id" json:"solicitante_id,omitempty"`
-  Autorizante		            string          `bson:"autorizante" json:"autorizante,omitempty"`
-  AutorizanteFecha          time.Time       `bson:"autorizanteFecha" json:"autorizanteFecha,omitempty"`
-	ImporteAutorizado					float64         `bson:"importeAutorizado" json:"importeAutorizado,omitempty"`
-	Timestamp	     			      time.Time				`bson:"timestamp" json:"timestamp"`
-}
-
-type ImporteSugerido struct {
-	Importe										float64         `bson:"importe" json:"importe"`
-}
+*/
