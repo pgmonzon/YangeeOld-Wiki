10977e0a36de4e4ac78c5df855ff82d32ef382f3
Patricio Garcia Monzon <pgmonzon@gmail.com>
2018-05-24 16:05:29 -0300

    Personal

--- models/personal.go
+++ models/personal.go
@@ -11,6 +11,10 @@ type Personal struct {
 	Empresa_id			      bson.ObjectId		`bson:"empresa_id" json:"empresa_id,omitempty"`
 	Apellido 				      string        	`bson:"apellido" json:"apellido"`
   Nombre                string          `bson:"nombre" json:"nombre"`
+	Celular								string					`bson:"celular" json:"celular"`
+	Direccion							string					`json:"direccion"`
+	Latitud  							string					`json:"latitud"`
+  Longitud 							string					`json:"longitud"`
   Categoria_id		      bson.ObjectId		`bson:"categoria_id" json:"categoria_id"`
 	Propio   				      bool  					`json:"propio"`
   BasicoSindicato_id		bson.ObjectId		`bson:"basicoSindicato_id" json:"basicoSindicato_id,omitempty"`
@@ -18,6 +22,8 @@ type Personal struct {
   Curso    				      time.Time				`json:"curso"`
 	LNH      				      time.Time				`json:"lnh"`
   Registro     				  time.Time				`json:"registro"`
+	FechaAlta							time.Time				`bson:"fechaAlta" json:"fechaAlta"`
+	FechaBaja							time.Time				`bson:"fechaBaja" json:"fechaBaja"`
 	Activo					      bool   					`json:"activo"`
 	Borrado   			      bool          	`bson:"borrado" json:"borrado"`
 	Timestamp			       	time.Time				`bson:"timestamp" json:"timestamp"`
