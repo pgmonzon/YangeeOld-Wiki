3c63142550a4640cc08e409d4f5a3f9f4a6293b7
Patricio Garcia Monzon <pgmonzon@gmail.com>
2018-06-01 11:18:04 -0300

    Liquidación

--- models/liquidacion.go
+++ models/liquidacion.go
@@ -6,30 +6,27 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-type Factura struct {
+type Liquidacion struct {
 	ID							          bson.ObjectId	 	`bson:"_id" json:"id,omitempty"`
 	Empresa_id			          bson.ObjectId		`bson:"empresa_id" json:"empresa_id,omitempty"`
-  Letra                     string          `bson:"letra" json:"letra"`
-  Suc                       string          `bson:"suc" json:"suc"`
-  Numero                    string          `bson:"numero" json:"numero"`
+  Liquidacion               string          `bson:"liquidacion" json:"liquidacion"`
   Fecha                     time.Time       `bson:"fecha" json:"fecha"`
-  Vencimiento               time.Time       `bson:"vencimiento" json:"vencimiento"`
-  Cliente_id	   	          bson.ObjectId		`bson:"cliente_id" json:"cliente_id"`
-  Cliente                   string          `bson:"cliente" json:"cliente"`
+  Transportista_id          bson.ObjectId		`bson:"transportista_id" json:"transportista_id"`
+  Transportista             string          `bson:"transportista" json:"transportista"`
   Descripcion               string          `bson:"descripcion" json:"descripcion"`
   Neto                      float64         `bson:"neto" json:"neto"`
   Iva105                    float64         `bson:"iva105" json:"iva105"`
   Iva21                     float64         `bson:"iva21" json:"iva21"`
   Total                     float64         `bson:"total" json:"total"`
   CuentaIngreso             string          `bson:"cuentaIngreso" json:"cuentaIngreso"`
-  Viajes                    []ViajesFact    `bson:"viajesFact" json:"viajesFact"`
-  FechaFacturacion          time.Time       `bson:"fechaFacturacion" json:"fechaFacturacion"`
-  UsuarioFacturacion_id     bson.ObjectId   `bson:"usuarioFacturacion_id" json:"usuarioFacturacion_id"`
-  UsuarioFacturacion        string          `bson:"usuarioFacturacion" json:"usuarioFacturacion"`
+  Viajes                    []ViajesLiq     `bson:"viajesLiq" json:"viajesLiq"`
+  FechaLiquidacion          time.Time       `bson:"fechaLiquidacion" json:"fechaLiquidacion"`
+  UsuarioLiquidacion_id     bson.ObjectId   `bson:"usuarioLiquidacion_id" json:"usuarioLiquidacion_id"`
+  UsuarioLiquidacion        string          `bson:"usuarioLiquidacion" json:"usuarioLiquidacion"`
 	Timestamp	     			      time.Time				`bson:"timestamp" json:"timestamp"`
 }
 
-type ViajesFact struct {
+type ViajesLiq struct {
 	Viaje_id		   	bson.ObjectId		`bson:"viaje_id" json:"viaje_id,omitempty"`
   FechaHora       time.Time       `bson:"fechaHora" json:"fechaHora"`
   Recorrido       string          `bson:"recorrido" json:"recorrido"`
