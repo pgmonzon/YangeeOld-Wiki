e503f670f91054d7cc982175b96b3fd7f957600b
Patricio <pgmonzon@gmail.com>
2018-10-22 20:24:39 -0300

    Remito Suc

--- handlers/sbr_RemitoSucursal.go
+++ handlers/sbr_RemitoSucursal.go
@@ -340,9 +340,19 @@ func SbrRemitoSucursalCancelar(w http.ResponseWriter, req *http.Request) {
 }
 
 func SbrRemitoSucursalAceptar(w http.ResponseWriter, req *http.Request) {
+  var documento models.SbrRemitoSucursalAceptarRechazar
   vars := mux.Vars(req)
   ID := vars["docID"]
   audit := "Aceptar"
+
+  // Decode del JSON
+  // ***************
+  decoder := json.NewDecoder(req.Body)
+  err := decoder.Decode(&documento)
+  if err != nil {
+    core.RspMsgJSON(w, req, "ERROR", "JSON", "INVALID_PARAMS: JSON decode erróneo", http.StatusBadRequest)
+    return
+  }
 
   // Verifico el formato del campo ID
   // ********************************
@@ -367,7 +377,7 @@ func SbrRemitoSucursalAceptar(w http.ResponseWriter, req *http.Request) {
 
   // Modifico
   // ********
-  estado, valor, mensaje, httpStat = SbrRemitoSucursalModificarEstado(documentoID, documentoExistente, req, audit)
+  estado, valor, mensaje, httpStat = SbrRemitoSucursalAceptarRechazar(documentoID, documentoExistente, documento, req, audit)
   if httpStat != http.StatusOK {
     core.RspMsgJSON(w, req, estado, valor, mensaje, httpStat)
     return
@@ -381,9 +391,19 @@ func SbrRemitoSucursalAceptar(w http.ResponseWriter, req *http.Request) {
 }
 
 func SbrRemitoSucursalRechazar(w http.ResponseWriter, req *http.Request) {
+  var documento models.SbrRemitoSucursalAceptarRechazar
   vars := mux.Vars(req)
   ID := vars["docID"]
   audit := "Rechazar"
+
+  // Decode del JSON
+  // ***************
+  decoder := json.NewDecoder(req.Body)
+  err := decoder.Decode(&documento)
+  if err != nil {
+    core.RspMsgJSON(w, req, "ERROR", "JSON", "INVALID_PARAMS: JSON decode erróneo", http.StatusBadRequest)
+    return
+  }
 
   // Verifico el formato del campo ID
   // ********************************
@@ -408,7 +428,7 @@ func SbrRemitoSucursalRechazar(w http.ResponseWriter, req *http.Request) {
 
   // Modifico
   // ********
-  estado, valor, mensaje, httpStat = SbrRemitoSucursalModificarEstado(documentoID, documentoExistente, req, audit)
+  estado, valor, mensaje, httpStat = SbrRemitoSucursalAceptarRechazar(documentoID, documentoExistente, documento, req, audit)
   if httpStat != http.StatusOK {
     core.RspMsgJSON(w, req, estado, valor, mensaje, httpStat)
     return
@@ -419,6 +439,120 @@ func SbrRemitoSucursalRechazar(w http.ResponseWriter, req *http.Request) {
   s := []string{"Rechazaste envío de ", documentoExistente.DeSucursal}
   core.RspMsgJSON(w, req, "OK", documentoExistente.DeSucursal, strings.Join(s, ""), http.StatusAccepted)
   return
+}
+
+func SbrRemitoSucursalAceptarRechazar(documentoID bson.ObjectId, documentoModi models.SbrRemitoSucursal, documento models.SbrRemitoSucursalAceptarRechazar, req *http.Request, audit string) (string, string, string, int) {
+  var docStock models.SbrStock
+  coll := config.DB_SbrRemitoSucursal
+  empresaID := context.Get(req, "Empresa_id").(bson.ObjectId)
+
+  // Genero una nueva sesión Mongo
+  // *****************************
+  session, err, httpStat := core.GetMongoSession()
+  if err != nil {
+    return "ERROR", "GetMongoSession", err.Error(), httpStat
+  }
+  defer session.Close()
+
+  // Intento la modificación
+  // ***********************
+  collection := session.DB(config.DB_Name).C(coll)
+  selector := bson.M{"_id": documentoID, "empresa_id": empresaID}
+  updator := bson.M{
+    "$set": bson.M{
+      "estado": documentoModi.Estado,
+      "recibio_id": documento.Recibio_id,
+      "recibio": documento.Recibio,
+      "fechaRecepcion": time.Now(),
+      "timestamp": time.Now(),
+    },
+  }
+  err = collection.Update(selector, updator)
+  if err != nil {
+    s := []string{"INTERNAL_SERVER_ERROR: ", err.Error()}
+    return "ERROR", audit, strings.Join(s, ""), http.StatusInternalServerError
+  }
+
+  // Si es Aceptado hago los movimientos de stock
+  // ********************************************
+  if documentoModi.Estado == "Aceptado" {
+    for _, item := range documentoModi.Detalle {
+      // Ingreso primero en la sucursal destino
+      docStock.ID = ""
+      collectionStock := session.DB(config.DB_Name).C(config.DB_SbrStock)
+      collectionStock.Find(bson.M{"empresa_id": empresaID, "sucursal_id": documentoModi.ASucursal_id, "articulo_id": item.SbrArticulo_id}).One(&docStock)
+
+      if docStock.ID == "" {
+        var docActualizar models.SbrStock
+        objID2 := bson.NewObjectId()
+        docActualizar.ID = objID2
+        docActualizar.Empresa_id = empresaID
+        docActualizar.SbrSucursal_id = documentoModi.ASucursal_id
+        docActualizar.SbrSucursal = documentoModi.ASucursal
+        docActualizar.SbrArticulo_id = item.SbrArticulo_id
+        docActualizar.SbrArticulo = item.SbrArticulo
+        docActualizar.Cantidad = item.Cantidad
+
+        collectionActualizar := session.DB(config.DB_Name).C(config.DB_SbrStock)
+        err = collectionActualizar.Insert(docActualizar)
+        if err != nil {
+          s := []string{"INTERNAL_SERVER_ERRORdd: ", err.Error()}
+          return "ERROR", audit, strings.Join(s, ""), http.StatusInternalServerError
+        }
+      } else {
+        suma := docStock.Cantidad + item.Cantidad
+        selector := bson.M{"empresa_id": empresaID, "_id": docStock.ID}
+        updator := bson.M{"$set": bson.M{"cantidad": suma}}
+
+        collectionActualizar := session.DB(config.DB_Name).C(config.DB_SbrStock)
+        err = collectionActualizar.Update(selector, updator)
+        if err != nil {
+          s := []string{"INTERNAL_SERVER_ERRORhh: ", err.Error()}
+          return "ERROR", audit, strings.Join(s, ""), http.StatusInternalServerError
+        }
+      }
+
+      // Saco de la sucursal origen
+      docStock.ID = ""
+      collectionStock = session.DB(config.DB_Name).C(config.DB_SbrStock)
+      collectionStock.Find(bson.M{"empresa_id": empresaID, "sucursal_id": documentoModi.DeSucursal_id, "articulo_id": item.SbrArticulo_id}).One(&docStock)
+
+      if docStock.ID == "" {
+        var docActualizar models.SbrStock
+        objID2 := bson.NewObjectId()
+        docActualizar.ID = objID2
+        docActualizar.Empresa_id = empresaID
+        docActualizar.SbrSucursal_id = documentoModi.DeSucursal_id
+        docActualizar.SbrSucursal = documentoModi.DeSucursal
+        docActualizar.SbrArticulo_id = item.SbrArticulo_id
+        docActualizar.SbrArticulo = item.SbrArticulo
+        docActualizar.Cantidad = item.Cantidad * -1
+
+        collectionActualizar := session.DB(config.DB_Name).C(config.DB_SbrStock)
+        err = collectionActualizar.Insert(docActualizar)
+        if err != nil {
+          s := []string{"INTERNAL_SERVER_ERRORdd: ", err.Error()}
+          return "ERROR", audit, strings.Join(s, ""), http.StatusInternalServerError
+        }
+      } else {
+        suma := docStock.Cantidad - item.Cantidad
+        selector := bson.M{"empresa_id": empresaID, "_id": docStock.ID}
+        updator := bson.M{"$set": bson.M{"cantidad": suma}}
+
+        collectionActualizar := session.DB(config.DB_Name).C(config.DB_SbrStock)
+        err = collectionActualizar.Update(selector, updator)
+        if err != nil {
+          s := []string{"INTERNAL_SERVER_ERRORhh: ", err.Error()}
+          return "ERROR", audit, strings.Join(s, ""), http.StatusInternalServerError
+        }
+      }
+    }
+  }
+
+  // Está todo Ok
+  // ************
+  core.Audit(req, coll, documentoID, audit, documentoModi)
+  return "OK", audit, "Ok", http.StatusOK
 }
 
 func SbrRemitoSucursalModificarEstado(documentoID bson.ObjectId, documentoModi models.SbrRemitoSucursal, req *http.Request, audit string) (string, string, string, int) {
